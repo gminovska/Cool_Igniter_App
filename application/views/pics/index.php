0c4fdd094c95b6deb9e67f0b3b98a72bec873729
Gordana Minovska <ekogoca@gmail.com>
2017-04-28 11:57:31 -0700

    Visual integration and style improvements

--- application/views/pics/index.php
+++ application/views/pics/index.php
@@ -2,16 +2,22 @@
 $this->load->view($this->config->item('theme') . 'header');
 ?>
 <h2>So, you wanna see some <?php echo $title; ?> photos?</h2>
+<div class="img_row">
+
 <?php
+
 foreach($pics as $pic){
 
     $size = 'm';
     $photo_url = '
     http://farm'. $pic->farm . '.staticflickr.com/' . $pic->server . '/' . $pic->id . '_' . $pic->secret . '_' . $size . '.jpg';
 
-    echo "<img title='" . $pic->title . "' src='" . $photo_url . "' />";
+    echo "<div class='item well'><img class='pics' title='" . $pic->title . "' src='" . $photo_url . "' /><p>". $pic->title . "</p></div>";
  
 }
-
+?>
+</div>
+	
+<?php
 $this->load->view($this->config->item('theme') . 'footer');
 ?>
